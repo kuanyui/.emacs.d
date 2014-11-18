bc63c754922ad82bebbad5b3e96c7feea91c1c0a
kuanyui <azazabc123@gmail.com>
2014-11-19 00:09:15 +0800

    [rc-c] Something craps.

--- rc/rc-c.el
+++ rc/rc-c.el
@@ -13,12 +13,17 @@
 	    (defun c-compile-current-file ()
 	      (interactive)
 	      (save-buffer)
+	      (if (file-exists-p (file-name-base)) (delete-file (file-name-base)))
 	      (shell-command (format "gcc -Wall %s -o %s"
 				     (buffer-real-name)
 				     (file-name-base))))
 	    (define-key c-mode-map (kbd "<f5>") 'c-compile-current-file)
- 	    ;(add-to-list 'ac-sources 'ac-source-c-headers)
- 	    ;(add-to-list 'ac-sources 'ac-source-c-header-symbols t)
+					;(add-to-list 'ac-sources 'ac-source-c-headers)
+					;(add-to-list 'ac-sources 'ac-source-c-header-symbols t)
+	    (define-key c-mode-map (kbd "C-c M-n") 'highlight-symbol-at-point)
+	    (define-key c-mode-map (kbd "M-n")'highlight-symbol-next)
+	    (define-key c-mode-map (kbd "M-p")'highlight-symbol-prev)
+	    (define-key c-mode-map (kbd "C-c M-p") 'highlight-symbol-query-replace)
 	    ))
 
 
