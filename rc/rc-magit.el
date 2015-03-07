878368032a5386424f6f77b7b6a4e80fa4a331e8
kuanyui <azazabc123@gmail.com>
2015-03-07 14:13:08 +0800

    [rc-magit] Toggle --all for git log by default.

--- rc/rc-magit.el
+++ rc/rc-magit.el
@@ -9,7 +9,15 @@
 (global-set-key (kbd "C-x g l") 'magit-log)
 (define-key magit-mode-map (kbd "C-c d") 'magit-diff-staged)
 
+
+(defun magit-log-all ()
+  (interactive)
+  (magit-key-mode-popup-logging)
+  (magit-key-mode-toggle-option (quote logging) "--all"))
+(define-key magit-mode-map (kbd "l") 'magit-log-all)
+
 (require 'git-timemachine)
+
 
 (provide 'rc-magit)
 ;;; rc-magit.el ends here
