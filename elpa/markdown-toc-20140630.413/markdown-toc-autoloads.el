ecc2305a3e0b366a7ee028fb3e0abcd3684a1abc
kuanyui <azazabc123@gmail.com>
2015-05-12 11:17:08 +0800

    remove markdown-toc.el

--- elpa/markdown-toc-20140630.413/markdown-toc-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; markdown-toc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "markdown-toc" "markdown-toc.el" (21524 40874
-;;;;;;  543524 531000))
-;;; Generated autoloads from markdown-toc.el
-
-(autoload 'markdown-toc/version "markdown-toc" "\
-Markdown-toc version.
-
-\(fn)" t nil)
-
-(autoload 'markdown-toc/generate-toc "markdown-toc" "\
-Generate a TOC for markdown file at current position.
-Afterward, if a TOC is already present in the buffer, it will update it.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("markdown-toc-pkg.el") (21524 40875 27610
-;;;;;;  156000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; markdown-toc-autoloads.el ends here
