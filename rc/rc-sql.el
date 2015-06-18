04b78248f3c01b901bd6a1dbcf029efc362452d0
kuanyui <azazabc123@gmail.com>
2015-06-19 02:18:06 +0800

    [rc-sql] init.

---
+++ rc/rc-sql.el
@@ -0,0 +1,13 @@
+;;; rc-sql.el ---                                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  kuanyui
+
+;; Author: kuanyui <azazabc123@gmail.com>
+
+
+(eval-after-load "sql"
+  '(load-library "sql-indent"))
+
+
+(provide 'rc-sql)
+;;; rc-sql.el ends here
