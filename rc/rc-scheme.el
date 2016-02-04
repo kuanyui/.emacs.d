1777b0a7851af0d6a636c37a976163b524602571
kuanyui <azazabc123@gmail.com>
2016-02-05 01:24:17 +0800

    scheme ibuffer

--- rc/rc-scheme.el
+++ rc/rc-scheme.el
@@ -1,12 +1,7 @@
 ;;; rc-scheme.el ---                                 -*- lexical-binding: t; -*-
 
-(setq process-coding-system-alist
-      (cons '("guile" utf-8 . utf-8) process-coding-system-alist))
- 
-(setq scheme-program-name "guile")
-(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
-(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
 
-
+(add-hook 'scheme-mode #'rainbow-delimiters-mode)
+(setq geiser-default-implementation 'guile)
 (provide 'rc-scheme)
 ;;; rc-scheme.el ends here
