efb1a7c02f9f4d809e0c46fba0c84cdccdef7b45
kuanyui <azazabc123@gmail.com>
2016-03-12 01:23:14 +0800

    Add rc-json

---
+++ rc/rc-json.el
@@ -0,0 +1,49 @@
+;;; rc-json.el ---                                   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  kuanyui
+
+;; Author: kuanyui <azazabc123@gmail.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defun search-comic-info ()
+  (interactive)
+  (let ((url (url-encode-url
+              (concat "https://www.google.co.jp/search?q="
+                      (buffer-substring-no-properties
+                       (progn (right-char 1)
+                              (re-search-backward "\"" nil t)
+                              (right-char 1)
+                              (point))
+                       (progn (re-search-forward "\"" nil t)
+                              (point))
+                       )))))
+    (eww url)))
+
+(defun my-json-setup ()
+  (rainbow-delimiters-mode-enable)
+  (flycheck-mode 1)
+  (define-key json-mode-map (kbd "<f3>") 'search-comic-info))
+
+(add-hook 'json-mode-hook 'my-json-setup)
+
+(provide 'rc-json)
+;;; rc-json.el ends here
