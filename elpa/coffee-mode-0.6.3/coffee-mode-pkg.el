13043649cdf0e263e7c892ed9d2f3cbef44471f6
kuanyui <azazabc123@gmail.com>
2016-08-03 17:44:16 +0800

    update packages

---
+++ elpa/coffee-mode-0.6.3/coffee-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "coffee-mode" "0.6.3" "Major mode for CoffeeScript code" '((emacs "24.1") (cl-lib "0.5")) :url "http://github.com/defunkt/coffee-mode" :keywords '("coffeescript" "major" "mode"))
---
+++ elpa/jade-mode-20160525.741/jade-mode-autoloads.el
@@ -0,0 +1,26 @@
+;;; jade-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "jade-mode" "jade-mode.el" (22430 48295 39743
+;;;;;;  585000))
+;;; Generated autoloads from jade-mode.el
+
+(autoload 'jade-mode "jade-mode" "\
+Major mode for editing jade node.js templates
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.jade\\'" . jade-mode))
+
+(add-to-list 'auto-mode-alist '("\\.pug\\'" . jade-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; jade-mode-autoloads.el ends here
