c77a6e8ff7790b906985531468c5bc69e45be740
kuanyui <azazabc123@gmail.com>
2017-03-08 12:11:33 +0800

    serial-term is awesome

--- rc/rc-term.el
+++ rc/rc-term.el
@@ -1,17 +1,33 @@
 (require 'term)
 (term-set-escape-char ?\C-x)
+
 (defun my-serial-term ()
   (interactive)
   (serial-term "/dev/cu.usbserial" 115200))
 
-(defun my-serial-term-set-ip ()
+(defun my-serial-term-set-switch-ip ()
   (interactive)
   (term-send-raw-string "system")
   (term-send-raw-string "netstatus")
-  (term-send-raw-string (format "netsettingipv4 %s 255.255.254.0 192.168.16.254 8.8.8.8" (read-from-minibuffer "IP (130~139)" "192.168.16.")))
+  (term-send-raw-string (format "netsettingipv4 %s 255.255.254.0 192.168.16.254 8.8.8.8"
+                                (read-from-minibuffer "IP (130~139) " "192.168.16.")))
   (term-send-raw-string "netstatus")
   (term-send-raw-string "..")
   )
+
+(defun my-serial-term-set-ap-ip ()
+  (interactive)
+  (term-send-raw-string "/")
+  (term-send-raw-string "lan")
+  (term-send-raw-string "ip")
+  (term-send-raw-string (format "static lan1 %s 255.255.254.0"
+                                (read-from-minibuffer "IP (130~139) " "192.168.16.")))
+  (term-send-raw-string "/")
+  (term-send-raw-string "system")
+  (term-send-raw-string "save")
+  (term-send-raw-string "/")
+  )
+
 
 (provide 'rc-term)
 ;;; rc-term.el ends here
