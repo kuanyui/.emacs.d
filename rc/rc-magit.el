c5b09881084436ebcb724e2f306c893c5ddce650
kuanyui <azazabc123@gmail.com>
2017-03-22 12:18:30 +0800

    [magit] cleanup whitspace before status

--- rc/rc-magit.el
+++ rc/rc-magit.el
@@ -8,8 +8,13 @@
 (add-to-list 'load-path "~/.emacs.d/lisps/magit/lisp/")
 (require 'magit)
 
+(defun my-magit-status ()
+  (interactive)
+  (my-whitespace-cleanup)
+  (magit-status))
+
 (setq magit-auto-revert-mode t)
-(global-set-key (kbd "C-x g s") 'magit-status)
+(global-set-key (kbd "C-x g s") 'my-magit-status)
 (global-set-key (kbd "C-x g l") 'magit-log)
 (global-set-key (kbd "C-x g b") 'magit-blame)
 (define-key magit-mode-map (kbd "C-c d") 'magit-diff-staged)
