32f1598fed1d84d7a59117f24d987e31cb3e93ff
kuanyui <azazabc123@gmail.com>
2017-03-29 13:52:32 +0800

    [scss] 對不起我不該被生下來

--- rc/rc-css.el
+++ rc/rc-css.el
@@ -1,5 +1,17 @@
 ;;; rc-css.el ---                                    -*- lexical-binding: t; -*-
 
+;; font-lock in SCSS-mode is buggy and may freeze the whole Emacs. Example:
+;; https://gist.github.com/kuanyui/f07579d957ea2dda426b8034d78269ba
+;; So derive a new scss-mode as workaround...
+(define-derived-mode omg-scss-mode css-mode "OMG SCSS"
+  "Major mode to edit \"Sassy CSS\" files."
+  (setq-local comment-start "// ")
+  (setq-local comment-end "")
+  (setq-local comment-continue " *")
+  (setq-local comment-start-skip "/[*/]+[ \t]*")
+  (setq-local comment-end-skip "[ \t]*\\(?:\n\\|\\*+/\\)"))
+(add-to-list 'auto-mode-alist '("\\.scss$" . omg-scss-mode))
+
 ;;======================================================
 ;; Color code convert (derived from Xah Lee's CSS Mode)
 ;;======================================================
