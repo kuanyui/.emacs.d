c77dd1508a15229fb256d98065f4058e05f52721
kuanyui <azazabc123@gmail.com>
2017-04-07 13:39:22 +0800

    add firefox-controller

---
+++ elpa/firefox-controller-20160320.1147/firefox-controller-autoloads.el
@@ -0,0 +1,32 @@
+;;; firefox-controller-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "firefox-controller" "firefox-controller.el"
+;;;;;;  (22664 19555 0 0))
+;;; Generated autoloads from firefox-controller.el
+
+(autoload 'firefox-controller-remote-mode "firefox-controller" "\
+Enter `firefox-controller-remote-mode'.
+
+\(fn)" t nil)
+
+(autoload 'firefox-controller-direct-mode "firefox-controller" "\
+Enter `firefox-controller-direct-mode'.
+
+\(fn)" t nil)
+
+(autoload 'firefox-controller-send-key-sequence "firefox-controller" "\
+Send keys specified by KEY-SEQ-STR to firefox.
+
+\(fn KEY-SEQ-STR)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; firefox-controller-autoloads.el ends here
---
+++ elpa/firefox-controller-20160320.1147/firefox-controller-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "firefox-controller" "20160320.1147" "An improved Firefox controller" '((moz "0") (popwin "1.0.0") (cl-lib "0.5")) :url "https://github.com/cute-jumper/emacs-firefox-controller" :keywords '("extensions"))
