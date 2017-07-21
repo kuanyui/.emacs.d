ffae8e098b88130e224a5bfe52d0d656f4260052
kuanyui <azazabc123@gmail.com>
2017-07-21 15:53:38 +0800

    [rc-markdown] mmm-mode

--- rc/rc-markdown.el
+++ rc/rc-markdown.el
@@ -15,6 +15,26 @@
 (setq markdown-enable-math t)
 (setq markdown-command "/usr/lib/node_modules/marked/bin/marked")
 
+(require 'mmm-mode)
+(setq mmm-global-mode 'maybe)
+
+(setq mmm-parse-when-idle 't)
+
+(defun my-mmm-markdown-auto-class (lang &optional submode)
+  "Define a mmm-mode class for LANG in `markdown-mode' using SUBMODE.
+If SUBMODE is not provided, use `LANG-mode' by default."
+  (let ((class (intern (concat "markdown-" lang)))
+        (submode (or submode (intern (concat lang "-mode"))))
+        (front (concat "^```" lang "[\n\r]+"))
+        (back "^```"))
+    (mmm-add-classes (list (list class :submode submode :front front :back back)))
+    (mmm-add-mode-ext-class 'markdown-mode nil class)))
+
+;; Mode names that derive directly from the language name
+(mapc 'my-mmm-markdown-auto-class
+      '("awk" "bibtex" "c" "cpp" "css" "html" "latex" "lisp" "makefile"
+        "markdown" "python" "r" "ruby" "sql" "stata" "xml" "js"))
+
 ;; [TODO] 把markdown的outline搞得跟org-mode的key-binding接近一點
 
 (provide 'rc-markdown)
