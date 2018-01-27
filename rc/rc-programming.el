8a8010f6b5e4f13fb1c88d727a70f4ba74fc4cd8
onohiroko <azazabc123@gmail.com>
2018-01-27 20:08:38 +0800

    [rc-programming] Remove unneed anymore xml-mode settings

--- rc/rc-programming.el
+++ rc/rc-programming.el
@@ -139,13 +139,6 @@
 (defun all-css-modes() (css-mode) (rainbow-mode))
 (add-to-list 'auto-mode-alist '("\\.css$" . all-css-modes)) ;; Load both major and minor modes in one call based on file type
 
-(defun my-xml-mode () (rainbow-mode 1) (xml-mode))
-(add-to-list 'auto-mode-alist '("\\.xml$" . my-xml-mode))
-
-(add-hook 'nxml-mode-hook #'my-xml-mode)
-
-
-
 ;;======================================================
 ;; mmm-mode
 ;;======================================================
