b7047a70e706a94a82d577b784ac37ba442055a0
ono hiroko <azazabc123@gmail.com>
2018-05-04 09:50:43 +0800

    [json][qt] Auto-revert .model file

--- rc/rc-qt.el
+++ rc/rc-qt.el
@@ -1,5 +1,7 @@
 ;;; rc-qt.el ---                                     -*- lexical-binding: t; -*-
 
+(add-to-list 'auto-mode-alist '("\\.model\\'" . json-mode))
+(add-hook 'json-mode-hook 'auto-revert-mode)
 (add-to-list 'auto-mode-alist '("\\.qrc\\'" . xml-mode))
 (add-to-list 'auto-mode-alist '("qmldir\\'" . conf-mode))
 
