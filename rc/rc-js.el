f7cac79d105ac224dfcc1e04cafe37a076357b17
onohiroko <azazabc123@gmail.com>
2018-06-04 13:53:52 +0800

    [rc-js] Flycheck + ESLint: search the eslint in node_modules

--- rc/rc-js.el
+++ rc/rc-js.el
@@ -207,5 +207,20 @@
   (eshell-send-input ""))
 
 
+;; ======================================================
+;; Flycheck
+;; ======================================================
+
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
 (provide 'rc-js)
 ;;; rc-javascript.el ends here
