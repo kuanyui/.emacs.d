50c8f7d8ec34c54d93ce97c6699c7ff260657f18
ono ono <azazabc123@gmail.com>
2020-06-19 01:01:10 +0800

    Remove some suspicious font settings

--- init.el
+++ init.el
@@ -171,12 +171,3 @@
  '(web-mode-css-indent-offset 2)
  '(web-mode-markup-indent-offset 2)
  '(xclip-mode nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#303030" :foreground "#c6c6c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "ADBO" :family "Source Code Pro")))))
-
-(put 'dired-find-alternate-file 'disabled nil)
