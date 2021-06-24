306bf1f425265f0a0b89801f935cab5036fea656
ono ono <azazabc123@gmail.com>
2021-06-24 17:58:22 +0800

    [rc-shell] remove font-lock for shell because it break syntax highlight in org-mode

--- rc/rc-shell.el
+++ rc/rc-shell.el
@@ -5,20 +5,20 @@
 ;;======================================================
 
 ;;較完整地支援shell script語法高亮。
-(defface font-lock-system-command-face
-  '((((class color)) (:foreground "purple")))
-  "I am comment"
-  :group 'font-lock-faces)
+;; (defface font-lock-system-command-face
+;; '((((class color)) (:foreground "purple")))
+;; "I am comment"
+;; :group 'font-lock-faces)
 
-(defun font-lock-system-command (&optional limit)
-  ""
-  (and (search-forward-regexp "\\<[a-zA-Z\\-]+\\>" limit t)
-       (executable-find
-        (buffer-substring-no-properties (car (bounds-of-thing-at-point 'word))
-                                        (cdr (bounds-of-thing-at-point 'word))))))
-
-(font-lock-add-keywords 'sh-mode
-                        '((font-lock-system-command . 'font-lock-system-command-face)))
+;; (defun font-lock-system-command (&optional limit)
+;; ""
+;; (and (search-forward-regexp "\\<[a-zA-Z\\-]+\\>" limit t)
+;;      (executable-find
+;;       (buffer-substring-no-properties (car (bounds-of-thing-at-point 'word))
+;;                                       (cdr (bounds-of-thing-at-point 'word))))))
+;;
+;; (font-lock-add-keywords 'sh-mode
+;;                         '((font-lock-system-command . 'font-lock-system-command-face)))
 
 ;; One-key to run script with Bash
 (require 'sh-script)
