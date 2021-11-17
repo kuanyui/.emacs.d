8b65ca05e4848a06e9c2e00c340392bbbe149f36
ono ono <azazabc123@gmail.com>
2021-11-17 17:50:32 +0800

    feat: [rc-magit] Don't restrict commit log length.

--- rc/rc-magit.el
+++ rc/rc-magit.el
@@ -9,6 +9,8 @@
 (global-set-key (kbd "C-x g s") 'magit-status)
 (global-set-key (kbd "C-x g l") 'magit-log)
 (global-set-key (kbd "C-x g b") 'magit-blame)
+(setq git-commit-summary-max-length 600)
+(remove-hook 'git-commit-setup-hook 'git-commit-turn-on-auto-fill)
 ;; (global-git-gutter-mode +1)
 ;; (setq magit-diff-refine-hunk 'all)
 ;; (setq magit-last-seen-setup-instructions "1.4.0")
