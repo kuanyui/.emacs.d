a41a2dc2f57bf0946603a91b89349d66e9439448
ono ono <azazabc123@gmail.com>
2021-12-12 15:21:38 +0800

    [rc-basic] Set backup-directory to ~/.emacs.d/_backups

--- rc/rc-basic.el
+++ rc/rc-basic.el
@@ -877,5 +877,25 @@ mouse-1: Display Line and Column Mode Menu"))))))
 
 (setq tramp-default-method "ssh")
 
+;; ======================================================
+;; File Backup Path
+;; ======================================================
+(defvar --backup-directory (concat user-emacs-directory "_backups"))
+(if (not (file-exists-p --backup-directory))
+    (make-directory --backup-directory t))
+(setq backup-directory-alist `(("." . ,--backup-directory)))
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
+      )
+
+
 (provide 'rc-basic)
 ;;; basic.el ends here
