b612d13c8e97c1550bd8a2e44bd811f7722a09cc
ono ono <azazabc123@gmail.com>
2022-01-08 02:13:08 +0800

    [byte-compile-packages-in-elpa] update

--- byte-compile-packages-in-elpa.el
+++ byte-compile-packages-in-elpa.el
@@ -3,8 +3,15 @@
 
 (require 'find-lisp)
 
-(mapc
- (lambda (x) (byte-compile-file x))
- (find-lisp-find-files
-  (expand-file-name (concat (file-name-directory (or load-file-name buffer-file-name)) "elpa/"))
-  "\\.el$"))
+(let* (
+       (all-el-files (find-lisp-find-files
+		      (expand-file-name (concat (file-name-directory (or load-file-name buffer-file-name)) "elpa/"))
+		      "\\.el$"))
+       (el-files-needing-compile (delete-if
+				  (lambda (f) (file-exists-p (format "%sc" f)))
+				  all-el-files))
+       )
+
+  (mapc
+   (lambda (x) (ignore-errors (byte-compile-file x)))
+   el-files-needing-compile))
