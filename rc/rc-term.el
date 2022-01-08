0e20160ab4c99f7ec15dd1be37f188ccbd8da668
ono ono <azazabc123@gmail.com>
2022-01-08 10:19:12 +0800

    wtf

--- rc/rc-term.el
+++ rc/rc-term.el
@@ -1,6 +1,3 @@
-(require 'term)
-(term-set-escape-char ?\C-x)
-
 (defun my-serial-term ()
   (interactive)
   (let ((file (cl-find-if #'file-exists-p
@@ -10,35 +7,40 @@
         (message "No console interface detected. Forgot to plug it?")
       (serial-term file 115200))))
 
-(defun my-serial-term-set-switch-ip ()
-  (interactive)
-  (term-send-raw-string "system")
-  (term-send-raw-string "netstatus")
-  (term-send-raw-string (format "netsettingipv4 %s 255.255.254.0 192.168.16.254 8.8.8.8"
-                                (read-from-minibuffer "IP (130~139) " "192.168.16.")))
-  (term-send-raw-string "netstatus")
-  (term-send-raw-string "save")
-  (term-send-raw-string "..")
+(with-eval-after-load 'term
+  (require 'term)
+  (term-set-escape-char ?\C-x)
+
+
+  (defun my-serial-term-set-switch-ip ()
+    (interactive)
+    (term-send-raw-string "system")
+    (term-send-raw-string "netstatus")
+    (term-send-raw-string (format "netsettingipv4 %s 255.255.254.0 192.168.16.254 8.8.8.8"
+                                  (read-from-minibuffer "IP (130~139) " "192.168.16.")))
+    (term-send-raw-string "netstatus")
+    (term-send-raw-string "save")
+    (term-send-raw-string "..")
+    )
+
+  (defun my-serial-term-set-ap-ip ()
+    (interactive)
+    (term-send-raw-string "/")
+    (term-send-raw-string "lan")
+    (term-send-raw-string "ip")
+    (term-send-raw-string (format "static lan1 %s 255.255.254.0"
+                                  (read-from-minibuffer "IP (130~139) " "192.168.16.")))
+    (term-send-raw-string "/")
+    (term-send-raw-string "system")
+    (term-send-raw-string "save")
+    (term-send-raw-string "/")
+    )
+
+  (defun term-send-up    () (interactive) (term-send-raw-string "\e[A"))
+  (defun term-send-down  () (interactive) (term-send-raw-string "\e[B"))
+  (defun term-send-right () (interactive) (term-send-raw-string "\e[C"))
+  (defun term-send-left  () (interactive) (term-send-raw-string "\e[D"))
   )
-
-(defun my-serial-term-set-ap-ip ()
-  (interactive)
-  (term-send-raw-string "/")
-  (term-send-raw-string "lan")
-  (term-send-raw-string "ip")
-  (term-send-raw-string (format "static lan1 %s 255.255.254.0"
-                                (read-from-minibuffer "IP (130~139) " "192.168.16.")))
-  (term-send-raw-string "/")
-  (term-send-raw-string "system")
-  (term-send-raw-string "save")
-  (term-send-raw-string "/")
-  )
-
-(defun term-send-up    () (interactive) (term-send-raw-string "\e[A"))
-(defun term-send-down  () (interactive) (term-send-raw-string "\e[B"))
-(defun term-send-right () (interactive) (term-send-raw-string "\e[C"))
-(defun term-send-left  () (interactive) (term-send-raw-string "\e[D"))
-
 
 (provide 'rc-term)
 ;;; rc-term.el ends here
--- transient/history.el
+++ transient/history.el
@@ -1,1 +1,6 @@
-nil
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-pull nil)
+ (magit-push nil))
