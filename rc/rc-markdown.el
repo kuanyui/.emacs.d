359bb7621738f6687e096ba28ff544490ec4450e
ono ono <azazabc123@gmail.com>
2022-06-08 22:21:03 +0800

    markdown

--- rc/rc-markdown.el
+++ rc/rc-markdown.el
@@ -11,30 +11,38 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdown\\'" . markdown-mode))
 
+(with-eval-after-load 'markdown-mode
+  (define-key markdown-mode-map (kbd "M-<down>") 'move-text-down)
+  (define-key markdown-mode-map (kbd "M-<up>") 'move-text-up)
+  )
+
 (setq markdown-enable-math t)
 (setq markdown-command "/usr/lib/node_modules/marked/bin/marked")
+(setq markdown-fontify-code-blocks-natively t)
 
-(require 'mmm-mode)
-(setq mmm-global-mode 'maybe)
+(setq markdown-enable-highlighting-syntax t)
+(setq markdown-fontify-whole-heading-line t)
+;; (require 'mmm-mode)
+;; (setq mmm-global-mode 'maybe)
 
-(setq mmm-parse-when-idle 't)
+;; (setq mmm-parse-when-idle 't)
 
-(defun my-mmm-markdown-auto-class (lang &optional submode)
-  "Define a mmm-mode class for LANG in `markdown-mode' using SUBMODE.
-If SUBMODE is not provided, use `LANG-mode' by default."
-  (let ((class (intern (concat "markdown-" lang)))
-        (submode (or submode (intern (concat lang "-mode"))))
-        (front (concat "^```" lang "[\n\r]+"))
-        (back "^```"))
-    (mmm-add-classes (list (list class :submode submode :front front :back back)))
-    (mmm-add-mode-ext-class 'markdown-mode nil class)
-    (mmm-add-mode-ext-class 'gfm-mode nil class)
-    ))
+;; (defun my-mmm-markdown-auto-class (lang &optional submode)
+;;   "Define a mmm-mode class for LANG in `markdown-mode' using SUBMODE.
+;; If SUBMODE is not provided, use `LANG-mode' by default."
+;;   (let ((class (intern (concat "markdown-" lang)))
+;;         (submode (or submode (intern (concat lang "-mode"))))
+;;         (front (concat "^```" lang "[\n\r]+"))
+;;         (back "^```"))
+;;     (mmm-add-classes (list (list class :submode submode :front front :back back)))
+;;     (mmm-add-mode-ext-class 'markdown-mode nil class)
+;;     (mmm-add-mode-ext-class 'gfm-mode nil class)
+;;     ))
 
-;; Mode names that derive directly from the language name
-(mapc 'my-mmm-markdown-auto-class
-      '("awk" "bibtex" "c" "cpp" "css" "html" "latex" "lisp" "makefile"
-        "markdown" "python" "r" "ruby" "sql" "stata" "xml" "js"))
+;; ;; Mode names that derive directly from the language name
+;; (mapc 'my-mmm-markdown-auto-class
+;;       '("awk" "bibtex" "c" "cpp" "css" "html" "latex" "lisp" "makefile"
+;;         "markdown" "python" "r" "ruby" "sql" "stata" "xml" "js"))
 
 (defalias 'cpp-mode 'c++-mode)
 ;; [TODO] 把markdown的outline搞得跟org-mode的key-binding接近一點
