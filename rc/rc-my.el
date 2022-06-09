244b3ca93a1c809509b13f961e9864c4702de5bf
ono ono <azazabc123@gmail.com>
2022-06-09 10:21:49 +0800

    startup

--- rc/rc-my.el
+++ rc/rc-my.el
@@ -82,7 +82,10 @@
 ;; (setq moe-theme-resize-title-markdown '(1.5 1.4 1.3 1.2 1.0 1.0))
 ;; (setq moe-theme-resize-title-org '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
 ;; (setq moe-theme-resize-title-rst '(1.5 1.4 1.3 1.2 1.1 1.0))
-(load-theme 'moe-dark t nil)
+(if (string-prefix-p (expand-file-name "~/company") (expand-file-name default-directory))
+    (load-theme 'moe-light t nil)
+  (load-theme 'moe-dark t nil)
+  )
 ;; (moe-theme-apply-color 'cyan)
 ;; (moe-dark)
 
