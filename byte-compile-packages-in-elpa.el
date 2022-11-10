8b11f34ce42dcf6b1c665b5d4862fd4e236de461
ono ono <azazabc123@gmail.com>
2022-11-10 22:13:31 +0800

    [byte-compile-packages-in-elpa.el] also build lisps/

--- byte-compile-packages-in-elpa.el
+++ byte-compile-packages-in-elpa.el
@@ -3,15 +3,20 @@
 
 (require 'find-lisp)
 
-(let* (
-       (all-el-files (find-lisp-find-files
-		      (expand-file-name (concat (file-name-directory (or load-file-name buffer-file-name)) "elpa/"))
-		      "\\.el$"))
-       (el-files-needing-compile (delete-if
-				  (lambda (f) (file-exists-p (format "%sc" f)))
-				  all-el-files))
-       )
+(defun byte-compile-dir-recursively (dir-root)
+  (let* (
+	 (all-el-files (find-lisp-find-files
+			(expand-file-name (concat (file-name-directory (or load-file-name buffer-file-name)) dir-root))
+			"\\.el$"))
+	 (el-files-needing-compile (delete-if
+				    (lambda (f) (file-exists-p (format "%sc" f)))
+				    all-el-files))
+	 )
 
-  (mapc
-   (lambda (x) (ignore-errors (byte-compile-file x)))
-   el-files-needing-compile))
+    (mapc
+     (lambda (x) (ignore-errors (byte-compile-file x)))
+     el-files-needing-compile)))
+
+(mapc
+ #'byte-compile-dir-recursively
+ '("lisps/" "elpa/"))
