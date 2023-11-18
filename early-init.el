bf503ee31d16a561fa80260b91eb0935f8a4e862
ono ono <azazabc123@gmail.com>
2023-11-18 21:38:48 +0800

    [early-init.el] wtf still doesn't work

---
+++ early-init.el
@@ -0,0 +1,10 @@
+;;; early-init.el ---                                -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  ono
+
+;; Author: ono <ono@ono-surfacego3>
+
+
+;; https://stackoverflow.com/questions/13233285/howto-disable-the-emacs-site-start-files-permanently
+(setq site-run-file nil)
+(setq inhibit-default-init t)
