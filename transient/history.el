ea069a07cecef284e6b2d274c1ba587dbba47ca2
ono ono <azazabc123@gmail.com>
2024-01-21 02:41:25 +0800

    remove hlinum

--- transient/history.el
+++ transient/history.el
@@ -1,13 +1,17 @@
-((magit-branch nil)
+((magit-blame
+  ("-w"))
+ (magit-branch nil)
  (magit-commit nil)
+ (magit-dispatch nil)
  (magit-fetch nil)
  (magit-log
-  ("-n256" "--graph" "--decorate")
-  (("--" "./")))
+  ("-n256" "--graph" "--decorate"))
  (magit-merge nil)
  (magit-pull nil)
  (magit-push nil
 	     ("--force-with-lease"))
- (magit-rebase nil
-	       ("--autostash"))
- (magit-reset nil))
+ (magit-rebase
+  ("--autostash")
+  nil)
+ (magit-submodule nil
+		  ("--recursive")))
