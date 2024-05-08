8919fcadf55c4a8838726ed353f722377fb7856e
ono ono <azazabc123@gmail.com>
2024-05-08 23:03:07 +0800

    recentz update

--- rc/rc-my.el
+++ rc/rc-my.el
@@ -195,10 +195,9 @@
 ;; ======================================================
 
 (require 'recentz)
-(global-set-key (kbd "C-x C-r") 'recentz-files)
-(global-set-key (kbd "C-x C-d") 'recentz-directories)
-(global-set-key (kbd "C-x C-p") 'recentz-projects)
-(global-set-key (kbd "C-x C-S-p") 'recentz-find-file-in-project)
+(global-set-key (kbd "C-x C-r") 'helm-recentz-files)
+(global-set-key (kbd "C-x C-d") 'helm-recentz-directories)
+(global-set-key (kbd "C-x C-p") 'helm-recentz-projects)
 (setq recentz-max-history
       '((files . 750)
 	(directories . 150)
