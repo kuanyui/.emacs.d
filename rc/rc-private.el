c7db65769ae4e7f047ac2504893760fdc9d43814
ono ono <azazabc123@gmail.com>
2025-09-30 20:09:43 +0800

    feat: [rc-private.el] more generic way to load files

--- rc/rc-private.el
+++ rc/rc-private.el
@@ -1,17 +1,11 @@
 ;;; rc-private.el ---                                -*- lexical-binding: t; -*-
 
-;;======================================================
-;; 私人的東西
-;;======================================================
-
-(mapcar (lambda (x)
-          (when (file-exists-p x)
-            (load-file x)))
-        '("~/.emacs.d/private/school.el"
-          "~/.emacs.d/private/twittering-filter-users.el"
-          "~/.emacs.d/private/flickr.el"
-          "~/.emacs.d/private/family-birthday.el"))
-
+(let ((private-dir "~/.emacs.d/private"))
+  (if (file-exists-p private-dir)
+      (mapcar (lambda (x)
+                (load-file x)
+                x)
+              (directory-files private-dir t "\\.el\\'"))))
 
 (provide 'rc-private)
 ;;; rc-private.el ends here
