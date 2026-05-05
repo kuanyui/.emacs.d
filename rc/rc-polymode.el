a1de9322d094b7831b135d5e9afa7ef4599901a5
ono ono <azazabc123@gmail.com>
2026-05-05 19:32:18 +0800

    feat: [rc-polymode.el] Use polymode to enhance jinja2-mode

--- rc/rc-polymode.el
+++ rc/rc-polymode.el
@@ -4,10 +4,28 @@
 
 (require 'polymode)
 
+;; ======================================================
+;; Host Mode
+;; ======================================================
+
 (define-hostmode poly-dockerfile-hostmode
   :mode 'dockerfile-mode)
 
-(define-innermode poly-jinja2-docker-innermode
+(define-hostmode poly-elisp-hostmode
+  :mode 'emacs-lisp-mode)
+
+(define-hostmode poly-sh-hostmode
+  :mode 'sh-mode)
+
+(define-hostmode poly-apparmor-hostmode
+  :mode 'apparmor-mode)
+
+;; ======================================================
+;; Inner Mode
+;; ======================================================
+
+
+(define-innermode poly-jinja2-innermode
   :mode 'jinja2-mode
   :head-matcher "{[{%]"
   :tail-matcher "[}%]}"
@@ -15,11 +33,56 @@
   :tail-adjust-face '(:foreground "#ee88ff" :weight bold)
   :fallback-mode 'host)
 
+(define-innermode poly-jinja2-raw-innermode
+  :mode nil                             ; use (inherit) host mode
+  :head-matcher "{%-? *raw *-?%}"
+  :tail-matcher "{%-? *endraw *-?%}"
+  :head-adjust-face '(:foreground "#ff88ff" :weight bold)
+  :tail-adjust-face '(:foreground "#ff88ff" :weight bold)
+  )
+
+(define-innermode poly-jinja2-comment-innermode
+  :mode nil                          ; inherit from host mode
+  :head-matcher "{#-?"
+  :tail-matcher "-?#}"
+  :head-adjust-face 'font-lock-comment-delimiter-face
+  :tail-adjust-face 'font-lock-comment-delimiter-face
+  :adjust-face 'font-lock-comment-face
+  :body-indent-offset 0
+  :pre-indent-offset 0
+  )
+
+;; ======================================================
+;; Poly Mode
+;; ======================================================
+
 (define-polymode poly-docker-jinja2-mode
   :hostmode 'poly-dockerfile-hostmode
-  :innermodes '(poly-jinja2-docker-innermode))
+  :innermodes '(poly-jinja2-comment-innermode poly-jinja2-raw-innermode poly-jinja2-innermode))   ; order-sensitive
 
-(add-to-list 'auto-mode-alist '("\\.\\(containerfile\\|dockerfile\\)\\.j2\\'" . poly-docker-jinja2-mode))
-(add-to-list 'auto-mode-alist '("\\(Containerfile\\|Dockerfile\\)\\.j2\\'" . poly-docker-jinja2-mode))
+(define-polymode poly-elisp-jinja2-mode
+  :hostmode 'poly-elisp-hostmode
+  :innermodes '(poly-jinja2-comment-innermode poly-jinja2-raw-innermode poly-jinja2-innermode))
+
+(define-polymode poly-sh-jinja2-mode
+  :hostmode 'poly-sh-hostmode
+  :innermodes '(poly-jinja2-comment-innermode poly-jinja2-raw-innermode poly-jinja2-innermode))
+
+(define-polymode poly-apparmor-jinja2-mode
+  :hostmode 'poly-apparmor-hostmode
+  :innermodes '(poly-jinja2-comment-innermode poly-jinja2-raw-innermode poly-jinja2-innermode))
+
+;; ======================================================
+;; auto-mode-alist
+;; ======================================================
+
+(dolist (entry '(("\\.\\(containerfile\\|dockerfile\\)\\.j2\\'" . poly-docker-jinja2-mode)
+                 ("\\(Containerfile\\|Dockerfile\\)\\.j2\\'"    . poly-docker-jinja2-mode)
+                 ("\\.el\\.j2\\'"                               . poly-elisp-jinja2-mode)
+                 ("\\.zshrc\\.j2\\'"                            . poly-sh-jinja2-mode)
+                 ("\\.bashrc\\.j2\\'"                           . poly-sh-jinja2-mode)
+                 ("\\.aa\\.j2\\'"                               . poly-apparmor-jinja2-mode)
+                 ))
+  (add-to-list 'auto-mode-alist entry))
 
 (provide 'rc-polymode)
