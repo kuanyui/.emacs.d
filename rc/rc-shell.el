50a5fe7c7e9578378ffaff71dedfa1a0a6c035b3
ono ono <azazabc123@gmail.com>
2026-05-08 12:58:15 +0800

    feat: [rc-shell.el] enhance shell script syntax highlight.

--- rc/rc-shell.el
+++ rc/rc-shell.el
@@ -29,5 +29,23 @@
     (save-buffer)(shell-command (format "bash %s" (buffer-real-name))))
   )
 
+(defun my/sh-extra-font-lock ()
+  "Beef up shell-script-mode highlighting."
+  (let ((builtin-keywords (regexp-opt '("declare" "local" "readonly" "export" "unset" "typeset" "shift" "trap" "set" "eval" "exec" "source" "alias" "unalias") 'symbols)))
+    (font-lock-add-keywords
+     nil
+     `(("\\(\\$[A-Za-z_][A-Za-z0-9_]*\\)" 1 font-lock-variable-name-face t) ; $VAR (also inside strings)
+       ("\\(\\${[^}]+}\\)" 1 font-lock-variable-name-face t) ; ${VAR}, ${VAR:-default}, etc.
+       ("\\(\\$[0-9@*#?$!-]\\)" 1 font-lock-variable-name-face t) ; positional/special params: $1 $@ $# $? $$ $!
+       ("\\(\\$(\\|)\\)" 1 font-lock-preprocessor-face t) ; $(...) command substitution delimiters
+       ("`\\([^`]*\\)`" 0 font-lock-preprocessor-face t) ; backtick command substitution
+       ("\\([<>]&?[0-9]*\\|[0-9]*>>?\\|<<<?\\)" 1 font-lock-warning-face) ; redirection operators
+       (,builtin-keywords . font-lock-builtin-face) ; common builtins
+       ))))
+
+(add-hook 'sh-mode-hook #'my/sh-extra-font-lock)
+(add-hook 'shell-script-mode #'my/sh-extra-font-lock)
+(add-hook 'bash-ts-mode-hook #'my/sh-extra-font-lock)
+
 (provide 'rc-shell)
 ;;; rc-shell.el ends here
